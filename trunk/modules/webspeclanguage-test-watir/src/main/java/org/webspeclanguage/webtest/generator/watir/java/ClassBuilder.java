4817b720ce96684b7d6f48f9642320182d86aeea
gonzalogtesta@gmail.com <gonzalogtesta@gmail.com@a547bdf6-fcea-cb88-96ac-e0a6145f5710>
2011-01-24 01:24:37 +0000

    WebSpec-2: Watir Classes Refactor & Multi Browser support added

--- trunk/modules/webspeclanguage-test-watir/src/main/java/org/webspeclanguage/webtest/generator/watir/java/ClassBuilder.java
+++ trunk/modules/webspeclanguage-test-watir/src/main/java/org/webspeclanguage/webtest/generator/watir/java/ClassBuilder.java
@@ -1,4 +1,23 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.webspeclanguage.webtest.generator.watir.java;
+
+/**
+ * Class Builder for Ruby Language 
+ * 
+ * @author Gonzalo G. Testa
+ */
 
 
 public class ClassBuilder {
@@ -11,35 +30,35 @@ public class ClassBuilder {
   private StringBuffer getClassCodeBuffer() {
     return classCodeBuffer;
   }
-  
+
   public ClassBuilder() {
     this.setClassCodeBuffer(new StringBuffer());
   }
-  
+
   public String getClassCode(){
     return this.getClassCodeBuffer().toString();
   }
-  
+
   public void newLine(){
     this.getClassCodeBuffer().append("\n");
   }
-  
+
   public void newLine(int n) {
     for(int i=0;i<n;i++)
       this.newLine();
   }
-  
+
   public void newTab(){
     this.getClassCodeBuffer().append("\t");
   }
-  
+
   public void newTab(int n){
     for(int i=0;i<n;i++)
       this.newTab();
   }
-  
+
   // class definition
-  
+
   // start the class
   public void setRequired(String required){
     if (!required.isEmpty()){
@@ -47,52 +66,48 @@ public class ClassBuilder {
       this.newLine();
     }
   }
-  
+
   public void startClass(String require, String className,
           String extendsImplements){
-    
+
     if (require != null){     
       String[] requires = require.split(",");
-    
-    
+
+
       for (String r : requires){
         this.setRequired(r.trim());
       }
       this.newLine();
     }
-    
+
     this.getClassCodeBuffer().append("class ");
     this.getClassCodeBuffer().append(className.replace(" ", "_"));
-    
+
     if (extendsImplements != null){
-    
+
       if (!extendsImplements.isEmpty()){
         this.getClassCodeBuffer().append(" < ");
         this.getClassCodeBuffer().append(extendsImplements);
-        
+
       }
     }
-    
+
     this.newLine(2);
-//    this.startMethod("setup");
-//    this.setMethodStatement("$browser = Watir::Browser.new()");
-//    this.endMethod();
-    
-    
+
   }
-  
+
   // end class
   public void endClass() {
-    
+
     this.getClassCodeBuffer().append("end");
     this.newLine();
   }
 
   ////////////////////////////////////////////
-  
+
   // method definition
-  
-  
+
+
   //start method
   public void startMethod(String methodName) {
     this.newTab();
@@ -100,14 +115,14 @@ public class ClassBuilder {
     this.getClassCodeBuffer().append(methodName.replace(" ", "_"));
     this.newLine();
   }
-  
+
   // complete method
   public void setMethodStatement(String statement) {
     this.newTab(2);
     this.classCodeBuffer.append(statement);
     this.newLine();
   }
-  
+
   //end method
   public void endMethod() {
     this.newTab();
@@ -121,11 +136,11 @@ public class ClassBuilder {
       this.newLine();
     }
   }
-  
-  
+
+
   public void addStatement(String statement) {
     this.getClassCodeBuffer().append("\t\t");
     this.getClassCodeBuffer().append(statement);
   }
-  
+
 }
--- trunk/modules/webspeclanguage-test-watir/src/main/java/org/webspeclanguage/webtest/generator/watir/java/WatirRubyWebTestGenerator.java
+++ trunk/modules/webspeclanguage-test-watir/src/main/java/org/webspeclanguage/webtest/generator/watir/java/WatirRubyWebTestGenerator.java
@@ -1,3 +1,15 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.webspeclanguage.webtest.generator.watir.java;
 
 import java.util.HashMap;
@@ -19,205 +31,200 @@ import org.webspeclanguage.webtest.base.WebTestItemVisitor;
 import org.webspeclanguage.webtest.base.WebTestSuite;
 import org.webspeclanguage.webtest.base.WebTestVisitor;
 
+/**
+ * Watir Web Test Generator class 
+ * 
+ * @author Gonzalo G. Testa
+ */
+
 public class WatirRubyWebTestGenerator implements WebTestGenerator {
 
-	private static final String BROWSER_OBJ = "$browser.";
+  private static final String BROWSER_OBJ = "$browser.";
 
-	private ClassBuilder classBuilder;
+  private ClassBuilder classBuilder;
 
-	private WatirRubyExpressionGenerator expressionGenerator;
+  private WatirRubyExpressionGenerator expressionGenerator;
 
-	//private HashMap<ExpressionType, String> expressionTypeToJavaMapping;
+  private Map<String, ExpressionType> alreadyDefinedVariables;
 
-	private Map<String, ExpressionType> alreadyDefinedVariables;
+  private WatirDriver browser;
+  
+  public WatirRubyWebTestGenerator(){
+    //If Driver is not specified, Firefox is used by default
+    this(new WatirFirefox());
+  }
+  
+  public WatirRubyWebTestGenerator(WatirDriver aBrowserDriver){
+    this.alreadyDefinedVariables = new HashMap<String, ExpressionType>();
+    this.browser = aBrowserDriver;
+    this.classBuilder = new ClassBuilder(); 
+    this.expressionGenerator = new WatirRubyExpressionGenerator();
+  }
 
-	public WatirRubyWebTestGenerator(){
-		this.alreadyDefinedVariables = new HashMap<String, ExpressionType>();
-
-		this.classBuilder = new ClassBuilder(); 
-		this.expressionGenerator = new WatirRubyExpressionGenerator();
-		//this.expressionTypeToJavaMapping = new HashMap<ExpressionType, String>();
-	}
-
-	public String generateTest(WebTest webTest) {
-		return this.getClassCode(webTest);
-	}
-
-
-	public String getClassCode(WebTest aWebTest) {
-		aWebTest.accept(new WebTestVisitor() {
-
-			public Object visitSimpleWebTest(SimpleWebTest simpleWebTest) {
-				generateSimpleWebTest(simpleWebTest);
-				return null;
-			}
-
-			public Object visitWebTestSuite(WebTestSuite webTestSuite) {
-				generateWebTestSuite(webTestSuite);
-				return null;
-			}
-		});
-		return this.classBuilder.getClassCode();
-	}
+  public String generateTest(WebTest webTest) {
+    return this.getClassCode(webTest);
+  }
 
 
+  public String getClassCode(WebTest aWebTest) {
+    aWebTest.accept(new WebTestVisitor() {
 
-	public void generateWebTestSuite(WebTestSuite webTestSuite) {
-		this.startClassFor(webTestSuite);
-		for (WebTest webTest : webTestSuite) {
-			this.basicGenerateTest(webTest);
-		}
-		this.endClass();
-	}
+      public Object visitSimpleWebTest(SimpleWebTest simpleWebTest) {
+        generateSimpleWebTest(simpleWebTest);
+        return null;
+      }
 
-	private void basicGenerateTest(WebTest webTest) {
-		this.alreadyDefinedVariables.clear();
-
-		webTest.accept(new WebTestVisitor() {
-
-			public Object visitSimpleWebTest(SimpleWebTest simpleWebTest) {
-				startMethodFor(simpleWebTest);
-				computeMethodFor(simpleWebTest);
-				endMethod();
-				return null;
-			}
-
-			public Object visitWebTestSuite(WebTestSuite webTestSuite) {
-				for (WebTest webTest : webTestSuite) {
-					basicGenerateTest(webTest);
-				}
-				return null;
-			}
-		});
-	}
-
-	private void startClassFor(WebTest aWebTest) {
-		String importString = "rubygems,watir,test/unit";
-
-		this.classBuilder.startClass(importString, "TC_" + aWebTest.getName(), "Test::Unit::TestCase");
-
-		classBuilder.startMethod("setup");
-
-		classBuilder.setMethodStatement("$browser = Watir::Browser.new()");
-
-		for (WebTestItem item : aWebTest.getSetUpItems()) {
-			this.computeStatementsFor(item);
-		}
-		this.classBuilder.endMethod();
-
-	}
-
-	public void generateSimpleWebTest(SimpleWebTest simpleWebTest) {
-		this.startClassFor(simpleWebTest);
-		this.startMethodFor(simpleWebTest);
-		this.computeMethodFor(simpleWebTest);
-		this.endMethod();
-		this.endClass();
-	}
-
-
-	private void endClass() {
-		this.classBuilder.endClass();
-	}
-
-	private void endMethod() {
-		this.classBuilder.endMethod();
-	}
-
-	private void computeMethodFor(SimpleWebTest simpleWebTest) {
-		for (WebTestItem item : simpleWebTest.getItems()) {
-			this.computeStatementsFor(item);
-		}
-	}
-
-	private void computeStatementsFor(WebTestItem item) {
-		item.accept(new WebTestItemGenerator());
-	}
-
-	private void startMethodFor(SimpleWebTest simpleWebTest) {
-		classBuilder.startMethod("test" + simpleWebTest.getName());
-	}
-
-	//  private void startClassFor(SimpleWebTest simpleWebTest) {
-	//    this.classBuilder.startClass("rubygems,watir,test/unit", "TC_" + simpleWebTest.getName(), "Test::Unit::TestCase");
-	//  }
-
-
-	protected String generateStatementFor(Expression expression) {
-		return expressionGenerator.generate(expression);
-	}
-
-	private final class WebTestItemGenerator implements WebTestItemVisitor {
+      public Object visitWebTestSuite(WebTestSuite webTestSuite) {
+        generateWebTestSuite(webTestSuite);
+        return null;
+      }
+    });
+    return this.classBuilder.getClassCode();
+  }
 
 
 
-		public Object visitWebAssertTitle(WebAssertTitle assertTitle) {
-			Expression title = assertTitle.getTitle();
-			String assertStatement = generateStatementFor(title);
-			String statement = "assert(" + assertStatement + ", " + BROWSER_OBJ + "title())";
-			classBuilder.addStatementAndNewLine(statement);
-			return null;
-		}
+  public void generateWebTestSuite(WebTestSuite webTestSuite) {
+    this.startClassFor(webTestSuite);
+    for (WebTest webTest : webTestSuite) {
+      this.basicGenerateTest(webTest);
+    }
+    this.endClass();
+  }
 
-		public Object visitWebAssertExpression(WebAssertExpression webAssertExpression) {
-			String assertStatement = generateStatementFor(webAssertExpression.getExpression());
-			String statement = "assert(" + assertStatement + ")";
-			classBuilder.addStatementAndNewLine(statement);
-			return null;
-		}
+  private void basicGenerateTest(WebTest webTest) {
+    this.alreadyDefinedVariables.clear();
 
-		public Object visitWebCreateVariableFromExpression(WebCreateVariableFromExpression webCreateVariableFromExpression) {
-			ExpressionType expressionType = webCreateVariableFromExpression.getType();
-			//String typeAsJavaString = expressionTypeToJavaMapping.get(expressionType);
-			String expressionAsJavaString = generateStatementFor(webCreateVariableFromExpression.getExpression());
-			String variableName = webCreateVariableFromExpression.getVariableName();
-			String statement = null;
+    webTest.accept(new WebTestVisitor() {
 
-			if (alreadyDefinedVariables.containsKey(variableName)) {
-				if (alreadyDefinedVariables.get(variableName).equals(expressionType)) {
-					statement = variableName + " = " + expressionAsJavaString + "";
-				} else {
-					throw new IllegalStateException("Variable " + variableName + " is redefined with a different type");
-				}
+      public Object visitSimpleWebTest(SimpleWebTest simpleWebTest) {
+        startMethodFor(simpleWebTest);
+        computeMethodFor(simpleWebTest);
+        endMethod();
+        return null;
+      }
 
-			} else {
-				statement = variableName + " = " + expressionAsJavaString + "";
-				alreadyDefinedVariables.put(variableName, expressionType);
-			}
+      public Object visitWebTestSuite(WebTestSuite webTestSuite) {
+        for (WebTest webTest : webTestSuite) {
+          basicGenerateTest(webTest);
+        }
+        return null;
+      }
+    });
+  }
 
-			classBuilder.addStatementAndNewLine(statement);
-			return null;
-		}
+  private void startClassFor(WebTest aWebTest) {
+    String importString = "rubygems,"+browser.getImport()+",test/unit";
 
-		public Object visitWebOpenUrl(WebOpenUrl webOpenUrl) {
+    this.classBuilder.startClass(importString, "TC_" + aWebTest.getName(), "Test::Unit::TestCase");
 
-			String statement = BROWSER_OBJ + "goto(" + "\"" + webOpenUrl.getUrl() + "\"" + ")";
-			classBuilder.addStatementAndNewLine(statement);
+    classBuilder.startMethod("setup");
 
-			return null;
-		}
+    classBuilder.setMethodStatement(browser.getDriver());
 
-		public Object visitWebExpression(WebExpression webExpression) {
-			String statement = generateStatementFor(webExpression.getExpression());//+ ";";
-			classBuilder.addStatementAndNewLine(statement);
-			return null;
-		}
+    for (WebTestItem item : aWebTest.getSetUpItems()) {
+      this.computeStatementsFor(item);
+    }
+    this.classBuilder.endMethod();
 
-		public Object visitWebWaitPageToLoad(WebWaitPageToLoad webWaitPageToLoad) {
-			String statement = BROWSER_OBJ + "wait()";
-			classBuilder.addStatementAndNewLine(statement);
-			return null;
-		}
-	}
+  }
 
-	public void setStopPolicy() {
-		// TODO Auto-generated method stub
+  public void generateSimpleWebTest(SimpleWebTest simpleWebTest) {
+    this.startClassFor(simpleWebTest);
+    this.startMethodFor(simpleWebTest);
+    this.computeMethodFor(simpleWebTest);
+    this.endMethod();
+    this.endClass();
+  }
 
-	}
 
-	public void setPackageName(String string) {
-		// TODO Auto-generated method stub
+  private void endClass() {
+    this.classBuilder.endClass();
+  }
 
-	}
+  private void endMethod() {
+    this.classBuilder.endMethod();
+  }
+
+  private void computeMethodFor(SimpleWebTest simpleWebTest) {
+    for (WebTestItem item : simpleWebTest.getItems()) {
+      this.computeStatementsFor(item);
+    }
+  }
+
+  private void computeStatementsFor(WebTestItem item) {
+    item.accept(new WebTestItemGenerator());
+  }
+
+  private void startMethodFor(SimpleWebTest simpleWebTest) {
+    classBuilder.startMethod("test" + simpleWebTest.getName());
+  }
+
+  protected String generateStatementFor(Expression expression) {
+    return expressionGenerator.generate(expression);
+  }
+
+  private final class WebTestItemGenerator implements WebTestItemVisitor {
+
+
+
+    public Object visitWebAssertTitle(WebAssertTitle assertTitle) {
+      Expression title = assertTitle.getTitle();
+      String assertStatement = generateStatementFor(title);
+      String statement = "assert(" + assertStatement + ", " + BROWSER_OBJ + "title())";
+      classBuilder.addStatementAndNewLine(statement);
+      return null;
+    }
+
+    public Object visitWebAssertExpression(WebAssertExpression webAssertExpression) {
+      String assertStatement = generateStatementFor(webAssertExpression.getExpression());
+      String statement = "assert(" + assertStatement + ")";
+      classBuilder.addStatementAndNewLine(statement);
+      return null;
+    }
+
+    public Object visitWebCreateVariableFromExpression(WebCreateVariableFromExpression webCreateVariableFromExpression) {
+      ExpressionType expressionType = webCreateVariableFromExpression.getType();
+
+      String expressionAsJavaString = generateStatementFor(webCreateVariableFromExpression.getExpression());
+      String variableName = webCreateVariableFromExpression.getVariableName();
+      String statement = null;
+
+      if (alreadyDefinedVariables.containsKey(variableName)) {
+        if (alreadyDefinedVariables.get(variableName).equals(expressionType)) {
+          statement = variableName + " = " + expressionAsJavaString + "";
+        } else {
+          throw new IllegalStateException("Variable " + variableName + " is redefined with a different type");
+        }
+
+      } else {
+        statement = variableName + " = " + expressionAsJavaString + "";
+        alreadyDefinedVariables.put(variableName, expressionType);
+      }
+
+      classBuilder.addStatementAndNewLine(statement);
+      return null;
+    }
+
+    public Object visitWebOpenUrl(WebOpenUrl webOpenUrl) {
+
+      String statement = BROWSER_OBJ + "goto(" + "\"" + webOpenUrl.getUrl() + "\"" + ")";
+      classBuilder.addStatementAndNewLine(statement);
+
+      return null;
+    }
+
+    public Object visitWebExpression(WebExpression webExpression) {
+      String statement = generateStatementFor(webExpression.getExpression());//+ ";";
+      classBuilder.addStatementAndNewLine(statement);
+      return null;
+    }
+
+    public Object visitWebWaitPageToLoad(WebWaitPageToLoad webWaitPageToLoad) {
+      String statement = BROWSER_OBJ + "wait()";
+      classBuilder.addStatementAndNewLine(statement);
+      return null;
+    }
+  }
 
 }
